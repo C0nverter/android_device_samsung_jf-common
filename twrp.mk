361faedb52e16c1d5afd52afa471aad960775304
Vlad <tsvetkov_vlad@ukr.net>
2017-10-10 21:08:18 +0300

    Updt twrp.mk

--- twrp.mk
+++ twrp.mk
@@ -12,23 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_REAL_SDCARD := true
 LZMA_RAMDISK_TARGETS := recovery
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_VARIANT := twrp
 TARGET_RECOVERY_FSTAB := device/samsung/jf-common/rootdir/etc/twrp.fstab
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun%d/file"
-TW_BRIGHTNESS_PATH := "/sys/devices/platform/msm_fb.526593/leds/lcd-backlight/brightness"
-TW_DEFAULT_BRIGHTNESS := 162
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_CRYPTO := true
-TW_NO_EXFAT_FUSE := true
-TW_MAX_BRIGHTNESS := 255
-TW_MTP_DEVICE := "/dev/mtp_usb"
-TW_NO_LEGACY_PROPS := true
 TW_NO_REBOOT_BOOTLOADER := true
+TW_TARGET_USES_QCOM_BSP := true
+TW_NO_LEGACY_PROPS := true
